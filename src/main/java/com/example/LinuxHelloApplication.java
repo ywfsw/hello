8f2d30220c24642c5e452e9491c013d723af836d
Gakkiiiii <ywfswywfsw@gmail.com>
2022-05-27 14:22:04 +0800

    update

---
+++ src/main/java/com/example/LinuxHelloApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinuxHelloApplication.class, args);
+	}
+
+}
